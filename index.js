23e8c1919792ef6188c989426424044324045730
bakir <bakirbazarkul16@gmail.com>
2022-11-21 14:40:06 +0300

    btn_content height 77vh

---
+++ index.js
@@ -0,0 +1,13 @@
+let btn_content = document.querySelector(".btn_content");
+let target;
+
+btn_content.addEventListener("click",(event)=>{
+   
+   target =  event.target;
+   switch(target){
+    case "":
+
+   }
+
+
+});
